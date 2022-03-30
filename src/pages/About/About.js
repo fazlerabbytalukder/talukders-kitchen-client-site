e87ea43e13ba7d0df5de30ca027bdb768188e872
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-03-30 14:28:38 +0600

    about

---
+++ src/pages/About/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './About.css';
+import knife from '../../images/knife.png';
+import spoon from '../../images/banner-image-t-kitchen-spoon.png';
+
+const About = () => {
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                    <div className='text-end'>
+                    <h1 style={{color:'#DCCA87'}}>About Us</h1>
+                    <img className='rotate-spoon mb-3' width='50px' src={spoon} alt="" />
+                    <p>We are pleased to announce the return of “Talukder's Kitchen” at its new location. Come and enjoy the flavors of Bangladesh in our spacious and contemporary setting.</p>
+                    <button className='button-1'>Know More</button>
+                    </div>
+                </div>
+                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                    <img className='img-fluid' src={knife} alt="" />
+                </div>
+                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                    <div className='text-start'>
+                    <h1 style={{color:'#DCCA87'}}>Our History</h1>
+                    <img className='mb-3' width='50px' src={spoon} alt="" />
+                    <p>We are serve people in 90's. Our old treadition is to serve pople better food for better place and i also know as best biriyani house in Tangail.</p>
+                    <button className='button-1'>Know More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from '../../About/About';
 import Banner from '../../Banner/Banner';
 import Navigation from '../../Shared/Navigation/Navigation';
 
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Navigation></Navigation>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
