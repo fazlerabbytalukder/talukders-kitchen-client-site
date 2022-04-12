953db308ef6c4607669856b06d8e8c802a045c75
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-04-12 15:07:40 +0600

    find us button workable

--- src/pages/FindUs/FindUs.js
+++ src/pages/FindUs/FindUs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import spoon from '../../images/banner-image-t-kitchen-spoon.png';
 import findFood from '../../images/food-talukder.png';
 
@@ -11,11 +12,11 @@ const FindUs = () => {
                         <small>Contact</small><br />
                         <img width='40px' src={spoon} alt="" />
                         <h1 className='mb-5' style={{color:'#DCCA87'}}>Find Us</h1>
-                        <p style={{color:'#DCCA87'}}>College Road, Bhuapur, Tangail (please visit our place for better food)</p>
+                        <p style={{color:'#DCCA87'}}>College Road, Bhuapur, Tangail ( please visit our place for better food )</p>
                         <h5 style={{color:'#DCCA87', fontWeight:'bolder'}}>Opening Hour</h5>
                         <p style={{color:'white'}}>Sat - thu 9:00 am - 9:00 pm</p>
                         <p className='mb-4' style={{color:'white'}}>Fri 4:00 pm - 9:00 pm</p>
-                        <button className='button-1'>Visit Us</button>
+                        <Link to='/contactUs'><button className='button-1'>Visit Us</button></Link>
                     </div>
                 </div>
                 <div className="col-md-6 mb-5">
